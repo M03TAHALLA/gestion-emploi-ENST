be3e43fe975f60bd6528043f2697ed834ae1cde9
Ahmed Mansof <ahmedmansof1@gmail.com>
2024-05-14 21:52:41 +0100

    ressources commit

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Http\Controllers\LoginController;
-use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\DepartementController;
