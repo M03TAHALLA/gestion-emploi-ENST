dbc532a91a7e6990567b914d65417363a54030d3
Ahmed Mansof <ahmedmansof1@gmail.com>
2024-05-22 01:51:37 +0100

    crud modules & filters

--- app/Models/Module.php
+++ app/Models/Module.php
@@ -5,8 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Enseignant extends Model
+class Module extends Model
 {
-    protected $table = 'enseignant';
     use HasFactory;
 }
